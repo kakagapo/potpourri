46363678b1b3718da8b434d60cf8e31289cc35a6
karna <429159+kakagapo@users.noreply.github.com>
2017-10-23 19:19:27 -0700

    ps script for learning ps

---
+++ powershell/learning.ps1
@@ -0,0 +1,29 @@
+﻿# list of all aliases
+Get-Alias 
+# specific info about ps alias 
+Get-Alias ps 
+
+# CMD comamnds
+ipconfig
+cls
+
+# pipe
+ipconfig | Out-File -FilePath C:\Users\kanavane\Desktop\ipconfig.txt; C:\Users\kanavane\Desktop\ipconfig.txt # the last part after ; opens the text file
+
+
+
+# Files
+
+# XML processing
+
+# PowerShell Remoting
+
+# Workflows
+ 
+# PowerShell 4.0 
+# Desired State Configuration(DSC) -> Deployment and configuration of Windows services and their settings
+
+# PowerShell 5.0
+# PowerShellGet -> to install and update PowerShell Modules
+# NetworkSwitch cmdlets
+# OneGet -> discover and install software packages from web
