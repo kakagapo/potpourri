69609bb25274cdcd5c47c3809acda611edec5709
karna <429159+kakagapo@users.noreply.github.com>
2019-04-05 10:37:54 -0700

    PInvoke demo

---
+++ dotnet/PInvokeDemo/PInvokeDemo/NativeMethods.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.Remoting;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PInvokeDemo
+{
+    class NativeMethods
+    {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool Beep(uint freq, uint dur);
+
+        // Todo: Add something that requires explicit marshalling as well.
+    }
+}
---
+++ dotnet/PInvokeDemo/PInvokeDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PInvokeDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NativeMethods.Beep(1000, 2000);
+        }
+    }
+}
