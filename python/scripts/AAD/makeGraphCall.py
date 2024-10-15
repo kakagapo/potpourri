b85d6522a699b92ea04f47bb5b3ca3f8603f581e
karna <429159+kakagapo@users.noreply.github.com>
2024-10-15 10:34:09 -0700

    Some python scripts for testing

--- python/scripts/AAD/makeGraphCall.py
+++ python/scripts/AAD/makeGraphCall.py
@@ -6,40 +6,44 @@ tenantId = input("Enter tenant ID: ")
 username = input("Enter username: ")
 password = getpass.getpass()
 
-# new endpoint = login.microsoftonline.com and 
-# old endpoint = login.windows.net
-tokenEndpoint = 'https://login.windows.net/%s/oauth2/token'%(tenantId)
-
 aadGraphDomainName = 'graph.windows.net'
 apiVersion = '1.6'
 userObjectId = input("Enter user object ID: ")
 
-def setProperty(tenantId, userObjectId, propertyName, propertyValue):
+def setProperty(tenantId, userObjectId, propertyName, propertyValue, accessToken):
     graphUrl = 'https://%s/%s/users/%s?api-version=%s'%(aadGraphDomainName,tenantId, userObjectId, apiVersion)
     payload = {propertyName: propertyValue}
     authorizationHeaderValue = 'Bearer %s'%(accessToken)
     headers = {'Authorization': authorizationHeaderValue}
     response = requests.patch(graphUrl, json=payload, headers=headers)
+    print("Request:")
     pprint(vars(response.request))
+    print("Response:")
     pprint(vars(response))
 
-payload = {
-    'grant_type': 'password',
-    'username': username,
-    'password': password,
-    'client_id': '1b730954-1685-4b74-9bfd-dac224a7b894', # AAD powershell client
-    'resource': 'https://%s'%(aadGraphDomainName)
-}
-x = requests.post(tokenEndpoint, data=payload)
-pprint(vars(x.request))
-pprint(vars(x))
+def getToken(tenantId):
+    payload = {
+        'grant_type': 'password',
+        'username': username,
+        'password': password,
+        'client_id': '1b730954-1685-4b74-9bfd-dac224a7b894', # AAD powershell client
+        'resource': 'https://%s'%(aadGraphDomainName)
+    }
+    tokenEndpoint = 'https://login.windows.net/%s/oauth2/token'%(tenantId)
+    tokenResponse = requests.post(tokenEndpoint, data=payload)
+    pprint(vars(tokenResponse))
+    if(tokenResponse.status_code != 200):
+        print("Unable to get token")
+        raise
+    else:
+        responseJson = tokenResponse.json()
+        accessToken = responseJson['access_token']
 
-if(x.status_code == 200):
-    responseJson = x.json()
-    accessToken = responseJson['access_token']
-    # make the required AAD graph call
-    setProperty(tenantId, userObjectId, "city", None)
-    setProperty(tenantId, userObjectId, "country", "United States")
+accessToken = getToken(tenantId)
+
+# make the required AAD graph call
+setProperty(tenantId, userObjectId, "city", None, accessToken)
+setProperty(tenantId, userObjectId, "country", "United States", accessToken)
     
 
 
